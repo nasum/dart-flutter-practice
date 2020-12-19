e6e3a5cd2da143c0d8da1845d11a05a4df9bf2fc
nasum <tomato.wonder.life@gmail.com>
2020-12-20 00:35:28 +0900

    ランダムの英字を表示するウィジットをstatefulのものにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -7,13 +9,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         title: 'DartFlutterPractice',
         home: Scaffold(
             appBar: AppBar(title: Text('DartFlutterPractice')),
-            body: Center(
-              child: Text(wordPair.asPascalCase),
-            )));
+            body: Center(child: RandomWords())));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
