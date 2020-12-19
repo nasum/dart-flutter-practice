f6baf44946e231323f49eb51a5c1edffaa167016
nasum <tomato.wonder.life@gmail.com>
2020-12-20 00:30:26 +0900

    helloworldの代わりに英語の文字を表示するようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
         title: 'DartFlutterPractice',
         home: Scaffold(
             appBar: AppBar(title: Text('DartFlutterPractice')),
             body: Center(
-              child: Text('Hello World'),
+              child: Text(wordPair.asPascalCase),
             )));
   }
 }
